75b7a5b9363914ceacac1b75b4b77aed87d74c16
blacksalman <salman51180189@gmail.com>
2023-06-17 11:55:04 +0530

    added reamde file

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+var axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin:true}))
+
+app.post('/authenticate', async (req, res)=>{
+    const { username } = req.body;
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            {username: username, secret: username, first_name: username},
+            {headers: {"private-key": "7792aa03-9f76-41ed-b41d-05f3320292ff"}}
+        );
+        return res.status(r.status).json(r.data)
+    } catch (error) {
+        return res.status(error.response.status).json(error.response.data)
+    }
+    
+})
+
+const PORT = process.env.PORT || 3500
+app.listen(PORT, ()=>console.log(`Server is running up....`))
