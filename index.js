349e56b223d2fbb6c1ce2eaa439f1fec729ce96c
blacksalman <salman51180189@gmail.com>
2023-06-17 12:14:10 +0530

    udpated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-var axios = require('axios');
+import express, { json } from 'express';
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors({origin:true}))
 
 app.post('/authenticate', async (req, res)=>{
